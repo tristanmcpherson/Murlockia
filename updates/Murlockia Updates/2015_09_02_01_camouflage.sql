ea5ba17a11de2fd9af2f3c7556ea8af5a06f1ae2
Flameshot <dragosgirip@yahoo.com>
2015-09-02 17:49:10 +0300

    DB/Spell: Hunter Camouflage

---
+++ updates/Murlockia Updates/2015_09_02_01_camouflage.sql
@@ -0,0 +1,5 @@
+-- Hunter Camouflage Scriptnames
+DELETE FROM spell_script_names where spell_id=(80326,51753);
+INSERT INTO spell_script_names VALUES
+(80326, "spell_hun_camouflage_periodic"),
+(51753, "spell_hun_camouflage");
