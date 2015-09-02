3add5dbf5cb1aabe09857f7c1f322ec865a5e998
Flameshot <dragosgirip@yahoo.com>
2015-09-03 02:40:26 +0300

    DB/Misc: Fix Warrior Charge Stun

---
+++ updates/Murlockia Updates/2015_09_03_01_charge_stun.sql
@@ -0,0 +1,3 @@
+-- Warrior Charge Stun Effect
+DELETE FROM spell_linked_spell WHERE spell_trigger=100 AND spell_effect=7922;
+INSERT INTO `spell_linked_spell` VALUES (100, 7922, 0, 'Warrior: Charge stun');
