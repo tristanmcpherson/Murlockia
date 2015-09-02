d8cb36d43af7ff62643bd49a61517b092cda76bd
Flameshot <dragosgirip@yahoo.com>
2015-09-02 18:26:32 +0300

    Fixed Typo

--- updates/Murlockia Updates/2015_09_02_01_camouflage.sql
+++ updates/Murlockia Updates/2015_09_02_01_camouflage.sql
@@ -1,5 +1,5 @@
 -- Hunter Camouflage Scriptnames
-DELETE FROM spell_script_names where spell_id=(80326,51753);
+DELETE FROM spell_script_names where spell_id IN (80326,51753);
 INSERT INTO spell_script_names VALUES
 (80326, "spell_hun_camouflage_periodic"),
 (51753, "spell_hun_camouflage");
