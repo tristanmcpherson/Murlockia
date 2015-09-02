cb2f091d2420a7bebec110da4e4b74131d2c1dfe
Flameshot <dragosgirip@yahoo.com>
2015-09-03 02:26:42 +0300

    DB/Spell: Fixed Hunter Glyph of KillShot

---
+++ updates/Murlockia Updates/2015_09_03_00_glyph_of_killshot.sql
@@ -0,0 +1,4 @@
+-- Glyph of Kill Shot
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_hun_glyph_kill_shot';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+('53351', 'spell_hun_glyph_kill_shot');
