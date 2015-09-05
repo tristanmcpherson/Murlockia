3435f35d20f4a1d56bcfbacf8bf7407e87439f3e
Flameshot <dragosgirip@yahoo.com>
2015-09-05 18:53:26 +0300

    Move lava lash on shaman.. Typo

--- src/server/scripts/Spells/spell_warlock.cpp
+++ src/server/scripts/Spells/spell_warlock.cpp
@@ -26,7 +26,6 @@
 #include "SpellScript.h"
 #include "SpellAuraEffects.h"
 #include "SpellAuras.h"
-#include "Unit.h""
 
 enum WarlockSpells
 {
@@ -1897,8 +1896,6 @@ public:
 	}
 };
 
-
-
 class spell_warl_pet_scaling_04 : public SpellScriptLoader
 {
 public:
@@ -2002,56 +1999,6 @@ public:
 	}
 };
 
-// Improved Lava Lash trigger
-class spell_sha_lava_lash_trigger : public SpellScriptLoader
-{
-    public:
-        spell_sha_lava_lash_trigger() : SpellScriptLoader("spell_sha_lava_lash_trigger") { }
-
-        class spell_sha_lava_lash_trigger_SpellScript : public SpellScript
-        {
-            PrepareSpellScript(spell_sha_lava_lash_trigger_SpellScript);
-
-            bool Load()
-            {
-                return GetCaster()->GetTypeId() == TYPEID_PLAYER;
-            }
-
-            void FilterTargets(std::list<WorldObject*>& unitList)
-            {
-                targets = unitList;
-                if (GetExplTargetUnit())
-                    targets.remove(GetExplTargetUnit());
-            }
-
-            void HandleOnHit()
-            {
-                if (Unit* target = GetHitUnit())
-                    if (Aura* flameShock = target->GetAura(8050, GetCaster()->GetGUID()))
-                        for (std::list<WorldObject*>::const_iterator itr = targets.begin(); itr != targets.end(); ++itr)
-                            if (Unit* triggerTarget = (*itr)->ToUnit())
-                                GetCaster()->AddAuraForTarget(flameShock, triggerTarget);
-            }
-
-
-            void Register()
-            {
-                OnObjectAreaTargetSelect += SpellObjectAreaTargetSelectFn(spell_sha_lava_lash_trigger_SpellScript::FilterTargets, EFFECT_0, TARGET_UNIT_DEST_AREA_ENEMY);
-                OnHit += SpellHitFn(spell_sha_lava_lash_trigger_SpellScript::HandleOnHit);
-            }
-
-        private:
-            std::list<WorldObject*> targets;
-
-        };
-
-        SpellScript* GetSpellScript() const
-        {
-            return new spell_sha_lava_lash_trigger_SpellScript();
-        }
-};
-
-
 void AddSC_warlock_spell_scripts()
 {
     new spell_warl_aftermath();
@@ -2087,5 +2034,4 @@ void AddSC_warlock_spell_scripts()
     new spell_warl_soul_swap_override();
     new spell_warl_soulshatter();
     new spell_warl_unstable_affliction();
-	new spell_sha_lava_lash_trigger();
 }
