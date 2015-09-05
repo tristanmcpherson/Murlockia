41bfa6ec4161819bc0d4ac54cd9da2085e6ecaac
Flameshot <dragosgirip@yahoo.com>
2015-09-05 13:29:19 +0300

    DB/Spell: Scriptname for Fulmination

---
+++ updates/Murlockia Updates/2015_09_05_01_fulmination.sql
@@ -0,0 +1,6 @@
+-- Fulmination - EarthShock
+
+DELETE FROM spell_script_names WHERE spell_id IN (8042,88766);
+INSERT INTO spell_script_names VALUES
+(8042, spell_sha_fulmination),
+(88766, spell_sha_fulmination);
