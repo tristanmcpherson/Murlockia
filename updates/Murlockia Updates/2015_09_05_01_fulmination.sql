c04182dd308b9f647a35054d446fe9c7e06fc727
Flameshot <dragosgirip@yahoo.com>
2015-09-05 18:35:32 +0300

    Fixed Typo

--- updates/Murlockia Updates/2015_09_05_01_fulmination.sql
+++ updates/Murlockia Updates/2015_09_05_01_fulmination.sql
@@ -2,5 +2,5 @@
 
 DELETE FROM spell_script_names WHERE spell_id IN (8042,88766);
 INSERT INTO spell_script_names VALUES
-(8042, spell_sha_fulmination),
-(88766, spell_sha_fulmination);
+(8042, "spell_sha_fulmination"),
+(88766, "spell_sha_fulmination");
--- updates/Murlockia Updates/2015_09_05_02_improoved_lava_lash.sql
+++ updates/Murlockia Updates/2015_09_05_02_improoved_lava_lash.sql
@@ -2,4 +2,4 @@
 
 DELETE FROM spell_script_names WHERE spell_id=105792;
 INSERT INTO spell_script_names VALUES
-(105792, new spell_sha_lava_lash_trigger);
+(105792, "spell_sha_lava_lash_trigger");
