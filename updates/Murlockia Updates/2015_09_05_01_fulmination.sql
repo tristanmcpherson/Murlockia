6a0e0f9a422a3aa3c6de3b6b2a1f25f61ad0af16
Flameshot <dragosgirip@yahoo.com>
2015-09-06 16:31:00 +0300

    Db/Spells: Shaman Unleash Elements

--- updates/Murlockia Updates/2015_09_05_01_fulmination.sql
+++ updates/Murlockia Updates/2015_09_05_01_fulmination.sql
@@ -1,6 +1,4 @@
 -- Fulmination - EarthShock
-
-DELETE FROM spell_script_names WHERE spell_id IN (8042,88766);
-INSERT INTO spell_script_names VALUES
-(8042, "spell_sha_fulmination"),
-(88766, "spell_sha_fulmination");
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (8042);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(8042, 'spell_sha_fulmination');
---
+++ updates/Murlockia Updates/2015_09_06_00_unlease_elements.sql
@@ -0,0 +1,4 @@
+-- Unleashed Elements
+DELETE FROM spell_script_names where scriptname='spell_sha_unleash_elements';
+INSERT INTO spell_script_names VALUES
+(73680,'spell_sha_unleash_elements');
